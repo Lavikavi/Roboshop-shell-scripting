14b577e28932de30ad937b30dbe3bb33a5d2633f
Kavitha316S <316kavithadhana@gmail.com>
2023-04-19 07:58:11 +0530

    Shell scripting for roboshop

---
+++ frontend.sh
@@ -0,0 +1,8 @@
+yum install nginx -y
+cp Roboshop.conf /etc/nginx/default.d/Roboshop.conf
+rm -rf /usr/share/nginx/html/*
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+systemctl restart nginx
+systemctl enable nginx
---
+++ user.sh
@@ -0,0 +1,15 @@
+yum install nodejs -y
+useradd roboshop
+mkdir /app
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
+cd /app
+unzip /tmp/user.zip
+npm install
+cp user.service /etc/systemd/system/user.service
+systemctl daemon-reload
+systemctl enable user
+systemctl start user
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+yum install mongodb-org-shell -y
+mongo --host mongodb-dev.devopsb62.online </app/schema/user.js

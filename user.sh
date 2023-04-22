5de3602973bb867e85936a909c794a0dfc1f8dc2
Kavitha316S <316kavithadhana@gmail.com>
2023-04-23 00:15:31 +0530

    Frontend

--- user.sh
+++ user.sh
@@ -1,15 +1,9 @@
-yum install nodejs -y
-useradd roboshop
-mkdir /app
-curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
-cd /app
-unzip /tmp/user.zip
-npm install
-cp user.service /etc/systemd/system/user.service
-systemctl daemon-reload
-systemctl enable user
-systemctl start user
+script=$(realpath "$0")
+script_path=$(dirname "$script")
+source ${script_path}/common.sh
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-yum install mongodb-org-shell -y
-mongo --host mongosh-dev.devopsb62.online </app/schema/user.js
+component=user
+schema_setup=mongo
+func_nodejs
+
+
